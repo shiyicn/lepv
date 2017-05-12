c1be65ebf2e1aa5f760e203d7e2c46334c913829
shiyicn <shiyi.cn11@gmail.com>
2017-05-12 19:10:05 +0200

    simplex algorithm

--- simplex.ml
+++ simplex.ml
@@ -2,6 +2,15 @@ module ST = Tree.SyntaxTree
 
 module SimplexSolver =
 struct
-    type tableau = ST.expr * ST.inv
-    
+    type t = ST.expr array
+
+    (*pivot operation
+    * t : canoical table with form
+            object : max z = sum (a_0i * x_i)
+            rows : sum_k (a_ki * x_i) >= 0
+    * i : pivot row index
+    * j : entering variable index
+    *)
+    let pivot t i j = ()
+
 end
